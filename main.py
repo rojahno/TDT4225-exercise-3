d9ca4890def408ea9b89da2a45a7d4197c0a0b60
Rojahno <andrersu@stud.ntnu.no>
2021-10-14 17:26:45 +0200

    Connected collections thrrough ids

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-import sys
-
-from DbConnector import DbConnector
 from task1.DatabaseSetup import DatabaseSetup
+import time
+import datetime
 
 
 def task_1():
+    start = time.time()
     # Creates a database setup object
     setup = DatabaseSetup()
-
     # Creates user, activites and track points
     # setup.drop_all_coll()
     # setup.create_all_collections()
     # Shows the collections
     # setup.show_coll()
-    setup.traverse_dataset()
+    # setup.traverse_dataset()
     setup.get_num_trackpoints()
+    print(f'Time elapsed: {str(datetime.timedelta(seconds=(time.time()-start)))}')
 
 
 def task_3():

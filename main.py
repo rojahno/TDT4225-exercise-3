030727afb96bae96b524672d0d3c59c090299451
Rojahno <andrersu@stud.ntnu.no>
2021-10-21 12:31:03 +0200

    Cleaned up code => removed redundant code and prints

--- main.py
+++ main.py
@@ -4,21 +4,13 @@ import datetime
 
 
 def task_1():
-    start = time.time()
-    # Creates a database setup object
-    setup = DatabaseSetup()
-    # Creates user, activites and track points
-    setup.drop_all_coll()
-    setup.create_all_collections()
-    # Shows the collections
-    # setup.show_coll()
-    setup.traverse_dataset()
-    setup.get_num_trackpoints()
-    print(f'Time elapsed: {str(datetime.timedelta(seconds=(time.time()-start)))}')
-
-
-def task_3():
-    print('task 3')
+    start = time.time()  # Starts the timer to see how much time has elapsed.
+    setup = DatabaseSetup()  # Creates a database setup object
+    setup.drop_all_coll()  # Drops the previous created collections
+    setup.create_all_collections()  # Creates user, activites and track points
+    setup.show_coll()  # Shows the collections
+    setup.traverse_dataset()  # Traverses the dataset and inserts data into the collections
+    print(f'Time elapsed: {str(datetime.timedelta(seconds=(time.time() - start)))}')  # Prints the time elapsed
 
 
 def main():
